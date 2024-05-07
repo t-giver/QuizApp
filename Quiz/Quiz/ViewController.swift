1f32f9bb2785e2de218823da2a8a10edaaf8766d
spark-05 <>
2024-05-07 16:01:53 +0900

    p167まで完了

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
-
+    @IBOutlet weak var nameTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let quizViewcontroller =
+            segue.destination as? QuizViewController {
+            if let text = self.nameTextField.text {
+                quizViewcontroller.nameText = text
+            }
+        }
     }
     
     
