574570d88646dab843da85a483898cf907fdf37c
spark-05 <>
2024-05-09 14:59:53 +0900

    p197 まで完了

--- Quiz/Quiz/QuizViewController.swift
+++ Quiz/Quiz/QuizViewController.swift
@@ -8,25 +8,57 @@
 import UIKit
 
 class QuizViewController: UIViewController {
+    
+    
+   
 
+    
+    
     @IBOutlet weak var label: UILabel!
     var nameText: String = ""
+    
+    @IBOutlet weak var quizCard: QuizCard!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.label.text = self.nameText
+        
+        self.quizCard.style = .initial
+        let panGestureReognizer =
+        UIPanGestureRecognizer(target: self, action: #selector(dragQuizCard(_:)))
+        self.quizCard.addGestureRecognizer(panGestureReognizer)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        @objc func dragQuizCard(_ sender: UIPanGestureRecognizer){
+            switch sender .state {
+            case .began, .changed:
+                self.transformQuizCard(gesture: sender)
+            case . ended:
+                break
+            default:
+                break
+            }
+        }
+        
+    func transformQuizCard(gesture: UIPanGestureRecognizer){
+        let translation = gesture.translation(in: self.quizCard)
+        let translationTransform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        let translationPercent = translation.x/UIScreen.main.bounds.width/2
+        let rotationAngle = CGFloat.pi/3 * translationPercent
+        let rotationTransform =
+        CGAffineTransform(rotationAngle: rotationAngle)
+        let transform =
+        translationTransform.concatenating(rotationTransform)
+        self.quizCard.transform = transform
+        
+        if translation.x > 0 {
+            self.quizCard.style = .right
+        } else {
+            self.quizCard.style = .wrong
+        }
+        
     }
-    */
-
+        
+    
 }
